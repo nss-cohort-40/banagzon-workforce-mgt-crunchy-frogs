9f2d1216d89dc9488791babd73484ae79f3bbf52
John Bain <johnknoxbain@gmail.com>
2020-08-11 09:48:17 -0500

    small fix to Employee List

--- database.session.sql
+++ database.session.sql
@@ -1,2 +1,11 @@
 INSERT INTO hrapp_employee(first_name, last_name, start_date, is_supervisor)
-VALUES("Zach", "Attack", '12-12-2019', False)
+VALUES("Zach", "Attack", '12-12-2019', False);
+
+
+select
+    e.id,
+    e.first_name,
+    e.last_name,
+    e.start_date,
+    e.is_supervisor
+from hrapp_employee e
---
+++ hrapp/views/Connection.py
@@ -0,0 +1,2 @@
+class Connection:
+    db_path = "/home/useradd/workspace/python/group-projects/banagzon-workforce-mgt-crunchy-frogs/db.sqlite3"
