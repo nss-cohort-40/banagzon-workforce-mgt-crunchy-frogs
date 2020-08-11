69f0a96c7dfd94e530122ba35321e62d431329bc
John Bain <johnknoxbain@gmail.com>
2020-08-11 13:27:24 -0500

    finished list and form for now, to checkout master

--- hrapp/views/employees/form.py
+++ hrapp/views/employees/form.py
@@ -19,15 +19,34 @@ def get_departments():
         return db_cursor.fetchall()
 
 
+def get_computers():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.manufacturer,
+            c.make,
+            ec.id connection_id,
+            ec.computer_id,
+            ec.employee_id
+        from hrapp_computer c
+        left join hrapp_employeecomputer ec on ec.computer_id = c.id;
+        """)
+
+        return db_cursor.fetchall()
+
 def employee_form(request):
     if request.method == 'GET':
         departments_computers = {
             "departments" : get_departments(),
-            "computers" : 
+            "computers" : get_computers()
         }
         template = "employees/form.html"
         context = {
-            "departments_computers": departments
+            "departments_computers": departments_computers
         }
 
         return render(request, template, context)
