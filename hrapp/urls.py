1fbe9410624e1fa18d8f046ff0443b11868e3b42
Zach Nicholson <raiste.doctow@gmail.com>
2020-08-12 13:27:43 -0500

    view program details and delete programs btn

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     path('employees/', employee_list, name='employee_list'),
     path('employees/form', employee_form, name='employee_form'),
     path('trainings/', trainings_list, name='trainings_list'),
+    path('trainings/<int:training_id>/', training_details, name='training'),
     path('trainings/past', trainings_list_past, name='trainings_list_past'),
     path('trainings/form', training_form, name='training_form'),
     path('departments/', department_list, name='department_list'),
     path('department/form', department_form, name='department_form'),
-    path('department/<int:department_id>/',
-         department_details, name='department_details'),
+    path('department/<int:department_id>/', department_details, name='department_details'),
 ]
--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -2,6 +2,7 @@ from .employees.employee_list import employee_list
 from .trainings.trainings_list import trainings_list
 from .trainings.trainings_list_past import trainings_list_past
 from .trainings.form import training_form
+from .trainings.training_details import training_details
 from .home import home
 from .auth.logout import logout_user
 from .employees.form import employee_form
