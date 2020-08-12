47ad6db2fb6909a60b2486afd9289f0666b3181d
felipetempus <felipemouradev@gmail.com>
2020-08-12 00:31:02 -0400

    view done

--- hrapp/views/employees/details.py
+++ hrapp/views/employees/details.py
@@ -5,35 +5,61 @@ from django.contrib.auth.decorators import login_required
 from hrapp.models import Employee, Department, Computer, EmployeeComputer, TrainingProgram, EmployeeTrainingProgram
 from ..connection import Connection
 
+def create_employee(cursor, row):
+    _row = sqlite3.Row(cursor, row)
 
-def get_book(book_id):
-    with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Book)
-        db_cursor = conn.cursor()
+    employee = Employee()
+    employee.id = _row["e_id"]
+    employee.first_name = _row["e_first_name"]
+    employee.last_name = _row["e_last_name"]
 
-        db_cursor.execute("""
-        SELECT
-            b.id,
-            b.title,
-            b.isbn,
-            b.author,
-            b.year_published,
-            b.librarian_id,
-            b.location_id
-        FROM libraryapp_book b
-        WHERE b.id = ?
-        """, (book_id,))
+    department = Department()
+    department.id = _row["d_id"]
+    department.name = _row["d_name"]
 
-        return db_cursor.fetchone()
+    computer = Computer()
+    computer.id = _row["c_id"]
+    computer.make = _row["c_make"]
+
+    training_program = TrainingProgram()
+    training_program.id = _row["tp_id"]
+    training_program.title = _row["tp_title"]
+
+    employee.department = department
+    employee.training_program = training_program
+
+    return employee
 
 @login_required
-def book_details(request, book_id):
+def get_employee(employee_id):
     if request.method == 'GET':
-        book = get_book(book_id)
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = create_employee
+            db_cursor = conn.cursor()
 
-        template = 'books/detail.html'
-        context = {
-            'book': book
-        }
+            db_cursor.execute("""
+            select
+                e.id as e_id,
+                e.first_name as e_first_name,
+                e.last_name as e_last_name,
+                e.department_id,
+                ec.id,
+                ec.computer_id,
+                ec.employee_id,
+                d.id as d_id,
+                d.dept_name as d_name,
+                c.id,
+                c.make,
+                etp.id,
+                etp.employee_id,
+                etp.training_program_id,
+            from hrapp_employee e
+            left join hrapp_department d on d.id = e.department_id
+            left join hrapp_employeecomputer ec on ec.employee_id = e.id
+            left join hrapp_computer c on ec.computer_id = c.id;
+            left join hrapp_employeetrainingprogram etp on etp.employee_id = e.id
+            left join hrapp_trainingprogram tp on etp.training_program_id = tp.id
+            """)
+            # TODO: Check the training_program_id
 
-        return render(request, template, context)
+            return db_cursor.fetchone()
