10871c6d40557fbf44dc69a4d9e06ef501756105
Zach Nicholson <raiste.doctow@gmail.com>
2020-08-13 10:19:58 -0500

    fetching branches

--- database.session.sql
+++ database.session.sql
@@ -3,3 +3,25 @@ VALUES("Demo Program", "This is a demo program for example purposes.", "2020-11-
 
 INSERT INTO hrapp_trainingprogram(title, description, start_date, end_date, capacity)
 VALUES("Test Training", "Welcome to Test Training, where your mind will be pushed to its limit.", "2015-05-15", "2015-06-15", 15);
+
+INSERT INTO hrapp_employeetrainingprogram(employee_id, training_program_id)
+VALUES(1, 3);
+INSERT INTO hrapp_employeetrainingprogram(employee_id, training_program_id)
+VALUES(2, 3);
+
+SELECT
+    t.id training_id,
+    t.title,
+    t.description,
+    t.start_date,
+    t.end_date,
+    t.capacity,
+    et.id,
+    et.employee_id,
+    et.training_program_id,
+    e.id,
+    e.first_name,
+    e.last_name
+FROM hrapp_trainingprogram t
+LEFT JOIN hrapp_employeetrainingprogram et ON et.training_program_id = t.id
+LEFT JOIN hrapp_employee e ON et.employee_id = e.id
--- hrapp/views/trainings/training_details_past.py
+++ hrapp/views/trainings/training_details_past.py
@@ -2,27 +2,12 @@ import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from hrapp.models import TrainingProgram
+from hrapp.models import TrainingProgram, Employee
 from ..connection import Connection
-
-
-def create_training(cursor, row):
-    _row = sqlite3.Row(cursor, row)
-
-    training = TrainingProgram()
-    training.id = _row["training_id"]
-    training.title = _row["title"]
-    training.description = _row["description"]
-    training.start_date = _row["start_date"]
-    training.end_date = _row["end_date"]
-    training.capacity = _row["capacity"]
-
-    return training
-
 
 def get_training(training_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = create_training
+        conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
@@ -32,12 +17,20 @@ def get_training(training_id):
             t.description,
             t.start_date,
             t.end_date,
-            t.capacity
+            t.capacity,
+            et.id,
+            et.employee_id,
+            et.training_program_id,
+            e.id,
+            e.first_name,
+            e.last_name
         FROM hrapp_trainingprogram t
+        LEFT JOIN hrapp_employeetrainingprogram et ON et.training_program_id = t.id
+        LEFT JOIN hrapp_employee e ON et.employee_id = e.id
         WHERE t.id = ?
         """, (training_id,))
 
-        return db_cursor.fetchone()
+        return db_cursor.fetchall()
 
 @login_required
 def training_details_past(request, training_id):
